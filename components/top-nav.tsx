b3a961e38943979909ec2fc29f8c50a6741a6d14
hernanferanndez <hernan.fernandez@qantas.com.au>
2024-07-03 11:22:43 +1000

    Cambio del texto a amarillo

--- components/top-nav.tsx
+++ components/top-nav.tsx
@@ -5,7 +5,7 @@ export const TopNav = () => (
   <nav className="container flex items-center justify-between w-full px-4 py-4 mx-auto lg:px-12">
     <Logo />
 
-    <ul className="flex items-center gap-4 text-sm font-medium text-white">
+    <ul className="flex items-center gap-4 text-sm font-medium text-yellow-100 font-serif">
 
       <li>
         <a href="https://www.instagram.com" rel="noopener noreferrer">
@@ -20,4 +20,4 @@ export const TopNav = () => (
       </li>
     </ul>
   </nav>
-)
+);
